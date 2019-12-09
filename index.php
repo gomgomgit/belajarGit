08593e048586d0439f33d38db25820c0ee4fa1b5
gomgomgit <thechocohead@gmail.com>
2019-12-09 09:55:16 +0700

    ngerapiin template

--- index.php
+++ index.php
@@ -30,9 +30,7 @@
         <small>it all starts here dude</small>
       </h1>
       <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Examples</a></li>
-        <li class="active">Blank page</li>
+        <li class="active"><i class="fa fa-home"></i> Home</li>
       </ol>
     </section>
 
